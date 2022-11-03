c71ad64b085cfd6da9d60a03ab4ff037f5499f1f
Ilina Tsanova <kniaginia.ili@gmail.com>
2022-11-03 20:18:50 +0200

    Added requirements

--- sortirane na char ili int masiv s macros.c
+++ sortirane na char ili int masiv s macros.c
@@ -1,3 +1,11 @@
+/*
+ * 6.Допълнителна задача:
+ * Направете макро функция, която да сортира масив от произволен тип, и да може да сортира низходящо или възходящо в зависимост от аргумент, подаден на макрото.
+ * Пример за дефиниция на подобно макро функция SORT(ARRAY, SIZE, TYPE, COMPARE).
+ * Пример за използване на подобна макро функция: SORT(array, 5, int, >).
+ * Хубаво е да се преизползва SWAP макро функцията от задача 5. Когато дефинирате макрото за да се чете по лесно кодът, можете да слагата “/” за да направите макрото на няколко реда.
+ */
+
 #include <stdio.h>
 
 #define COUNT 5
@@ -12,38 +20,38 @@
                 for (int i = 0; i < (SIZE - 1); i++)        \
                     if (ARRAY[i] COMPARE ARRAY[i + 1]) {    \
                         SWAP(ARRAY[i], ARRAY[i+1], TYPE);   \
-                    }                                       
-
-                                    
+                    }
 
 
-            
+
+
+
 int main()
 {
     int arr[COUNT] = {4, 5, 2, 1, 7};
     char str[COUNT] = {'d', 'a', 'c', 'e', 'b'};
-    
+
     SORT(str, COUNT, char, >);
     for (int i = 0; i < COUNT; i++)
         printf("%c ", str[i]);
-        
+
     printf("\n");
-    
+
     SORT(str, COUNT, char, <);
     for (int i = 0; i < COUNT; i++)
         printf("%c ", str[i]);
-        
+
     printf("\n");
-        
+
     SORT(arr, COUNT, int, >);
     for (int i = 0; i < COUNT; i++)
         printf("%d ", arr[i]);
-        
+
     printf("\n");
-        
+
     SORT(arr, COUNT, int, <);
     for (int i = 0; i < COUNT; i++)
         printf("%d ", arr[i]);
-        
+
     return 0;
 }
